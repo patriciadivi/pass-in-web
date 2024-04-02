31fbb51e449999971377e598c6c2d32137e5b738
PatriciaDivi <patriciavianad@gmail.com>
2024-04-02 19:06:48 -0300

    feat(DESENVOLVIMENTO): :recycle: Criando component personalizado para links de navegação

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,4 +1,5 @@
  import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
+import { NavLink } from './nav-link';
 
 export function Header() {
   return (
@@ -6,8 +7,9 @@ export function Header() {
       <img src={nlwUniteIcon} alt="Logo nlw Unite Icon" />
 
       <nav className="flex items-center gap-5">
-        <a href="" className="font-medium text-sm text-zinc-300">Eventos</a>
-        <a href="" className="font-medium text-sm">Participantes</a>
+        {/* <a href="" className="font-medium text-sm text-zinc-300">Eventos</a> */}
+        <NavLink href='/eventos'>Eventos</NavLink>
+        <NavLink href='/participantes'>participantes</NavLink>
       </nav>
     </div>
   );
